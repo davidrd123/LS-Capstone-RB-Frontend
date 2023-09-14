7bcf4159aa5a13d04aa8d8ec94731f4b3ea4edb2
David Dickinson <davidrd123@gmail.com>
2023-09-13 21:58:40 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { React, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import CreateBin from './components/CreateBin';
+import BinsList from './components/BinsList';
+import BinDetail from './components/BinDetail';
+import RequestDetail from './components/RequestDetail';
+
+import './App.css';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+const App = () => {
+
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/bins" element={<BinsList />} />
+          <Route path="/bins/:binId" element={<BinDetail />} />
+          <Route path="/bins/:binId/requests/:requestId" element={<RequestDetail />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
